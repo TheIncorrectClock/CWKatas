6e912144592b804cf922eb3ff2a78e0a4c21feb9
root <root@localhost>
2017-02-13 23:10:47 +0100

    First Version of Solution

--- AreSame/src/AreSame.java
+++ AreSame/src/AreSame.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class AreSame {
 
     public static boolean comp(int[] a, int[] b) {
-        return null;
+        if(a == null || b == null) {
+            return false;
+        }
+
+        if(a.length != b.length) {
+            return false;
+        }
+
+        List<Integer> values = new ArrayList<>(a.length);
+        for(Integer value : a) {
+            values.add(value * value);
+        }
+        Collections.sort(values, Integer::compareTo);
+
+        List<Integer> squares = new ArrayList<>(b.length);
+        for(Integer square : b) {
+            squares.add(square);
+        }
+        Collections.sort(squares, Integer::compareTo);
+
+        for(int i = 0; i < values.size(); i++) {
+            if(squares.get(i).intValue() != values.get(i).intValue()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- AreSame/test/AreSameTest.java
+++ AreSame/test/AreSameTest.java
@@ -1,11 +1,13 @@
-import static org.junit.Assert.*;
-        import org.junit.Test;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 public class AreSameTest {
 
     @Test
-    public void shouldSecondArrayContainSquareValuesOfFirstArray() {
+    public void shouldPassIfSecondArrayContainSquareValuesOfFirstArray() {
         int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
         int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
 
@@ -15,12 +17,60 @@ public class AreSameTest {
     }
 
     @Test
-    public void shouldSecondArrayNotContainSquareValuesOfFirstArray() {
+    public void shouldFailIfSecondArrayNotContainSquareValuesOfFirstArray() {
         int[] firstArray = new int[]{11, 4, 9};
         int[] secondArray = new int[]{16, 81, 120};
 
         boolean arraysAreNotSame = AreSame.comp(firstArray, secondArray);
 
         assertFalse(arraysAreNotSame);
+    }
+
+    @Test
+    public void shouldFailIfSecondArrayNotContainExactSquareValuesOfFirstArray() {
+        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 20736};
+
+        boolean arraysAreNotSame = AreSame.comp(a, b);
+
+        assertFalse(arraysAreNotSame);
+    }
+
+    @Test
+    public void shouldFailIfSecondArrayNotContainTheSameNumberOfValuesAsFirstArray() {
+        int[] firstArray = new int[]{1, 4, 11};
+        int[] secondArray = new int[]{1, 16, 121, 16};
+
+        boolean arraysAreNotSame = AreSame.comp(firstArray, secondArray);
+
+        assertFalse(arraysAreNotSame);
+    }
+
+    @Test
+    public void shouldFailIfOneOfArraysIsNull() {
+        int[] firstArray = null;
+        int[] secondArray = new int[]{11, 121, 4};
+
+        boolean arraysAreNotSame = AreSame.comp(firstArray, secondArray);
+
+        assertFalse(arraysAreNotSame);
+
+
+        firstArray = new int[]{11, 121, 4};
+        secondArray = null;
+
+        arraysAreNotSame = AreSame.comp(firstArray, secondArray);
+
+        assertFalse(arraysAreNotSame);
+    }
+
+    @Test
+    public void shouldPassIfBothArraysAreEmpty() {
+        int[] firstArray = new int[]{};
+        int[] secondArray = new int[]{};
+
+        boolean arraysAreSame = AreSame.comp(firstArray, secondArray);
+
+        assertTrue(arraysAreSame);
     }
 }
